c90e084ae95ea45a46c52a747c108cf02559515a
desiraj <desiree.aban93@gmail.com>
2020-02-02 16:07:37 +0100

    Created GenerateSQLInsertFile

---
+++ src/org/fileCreatorUtils/bloggerClient/GenerateSQLInsertFile.java
@@ -0,0 +1,15 @@
+package org.fileCreatorUtils.bloggerClient;
+
+/**
+ * 
+ * @author Desirée Abán
+ *
+ */
+public class GenerateSQLInsertFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
